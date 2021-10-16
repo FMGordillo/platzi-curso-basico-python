c2eb05567f48d9ccd5106cb2dd2f589830bad203
FMGordillo <facundomgordillo@gmail.com>
2021-10-15 23:07:10 -0300

    :wrench: Formatted correctly

--- dolar_conversor.py
+++ dolar_conversor.py
@@ -3,8 +3,7 @@ pesos = float(pesos)
 
 dolar_hoy = 182.00
 
-dolar_convertido = pesos / dolar_hoy
+dolar_convertido = round(pesos / dolar_hoy, 2)
 
 # TODO: Cual es mejor?
-print('Tenes US$%f' % (dolar_convertido))
-print('Tenes US$' + str(dolar_convertido))
+print('Tenes US$%s' % (dolar_convertido))
